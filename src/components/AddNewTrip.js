7a3dbcd5def8487bc71256491fa37cd5bcaf7190
Minh Nhat <minhnhat44.fr@gmail.com>
2023-04-24 21:08:03 +0300

    change the default to minus instead of the latest car

--- src/components/AddNewTrip.js
+++ src/components/AddNewTrip.js
@@ -40,7 +40,7 @@ export const AddNewTrip = () => {
 
         }
 
-        if (tripCarId === 0) {
+        if (tripCarId === -1) {
             alert("Please select a car!");
             return;
         } else {
@@ -58,63 +58,63 @@ export const AddNewTrip = () => {
 
     return (
         <>
-            <h2>Add new trip</h2>
-            <form onSubmit={onSubmit}>
-                <div className="form-control">
-                    <label htmlFor="tripCarId">Select the car</label>
-                    <select value={tripCarId} onChange={(e) => handleCarSelect(parseInt(e.target.value))}>
-                        {cars.map(car => (
-                            <option key={car.id} value={car.id}>
-                                {car.text} {car.isElectronic ? '(Electronic)' : ''}</option>
-                        ))}
-                    </select>
-                </div>
-                <h6> </h6>
-                <h6> </h6>
-                <h6> </h6>
+        <h2>Add new trip</h2>
+        <form onSubmit={onSubmit}>
+        <div className="form-control">
+        <label htmlFor="tripCarId">Select the car</label>
+        <select value={tripCarId} onChange={(e) => handleCarSelect(parseInt(e.target.value))}>
+        <option value={-1}>-</option>
+        {cars.map(car => (
+            <option key={car.id} value={car.id}>
+                {car.text} {car.isElectronic ? '(Electronic)' : ''}</option>
+        ))}
+        </select>
+        </div>
                 
-                {!currentCar || !currentCar.isElectronic ? (
-                    <>
-                        <h4>Petrol/diesel cars</h4>
-                        <div className="form-control">
-                            <label htmlFor="refuellingLiters">Refuelling in liters</label>
-                            <input type="number" value={refuellingLiters} onChange={(e) =>
-                                setRefuellingLiters(e.target.value)} placeholder="Enter refuelling liters..." />
-                        </div>
-                        <div className="form-control">
-                            <label htmlFor="refuellingCost">Refuelling cost in euros</label>
-                            <input type="number" value={refuellingCost} onChange={(e) =>
-                                setRefuellingCost(e.target.value)} placeholder="Enter refuelling cost..." />
-                        </div>
-                        <div className="form-control">
-                            <label htmlFor="distanceDrivenWithRefuelling">Distance driven with refuelling</label>
-                            <input type="number" value={distanceDrivenWithRefuelling} onChange={(e) =>
-                                setDistanceDrivenWithRefuelling(e.target.value)} placeholder="Enter distance driven with refuelling..." />
-                        </div>
-                    </>
-                ) : (
-                    <>
-                        <h4>Electric cars</h4>
-                        <div>
-                            <label htmlFor="chargingAmount">Recharing amount in kWh</label> 
-                            <input type="number" value={RechargingInkWh} onChange={(e) =>
-                                setechargingInkWh(e.target.value)} placeholder="Enter charing amount in kWh..." />
-                        </div>
-                        <div>
-                            <label htmlFor="chargingCost">Charging cost in euros</label>
-                            <input type="number" value={ChargingCost} onChange={(e) =>
-                                setChargingCost(e.target.value)} placeholder="Enter charging cost..." />
-                        </div>
-                        <div>
-                            <label htmlFor="DistanceDrivenWithCharge">Distance driven with a charge</label>
-                            <input type="number" value={distanceDrivenWithCharging} onChange={(e) =>
-                                setdistanceDrivenWithCharging(e.target.value)} placeholder="Enter distance driven with a charge..." />
-                        </div>
-                    </>
-                )}
+        {!currentCar || !currentCar.isElectronic ? (
+        <>
+        <h4>Petrol/diesel cars</h4>
+        <div className="form-control">
+        <label htmlFor="refuellingLiters">Refuelling in liters</label>
+        <input type="number" value={refuellingLiters} onChange={(e) =>
+            setRefuellingLiters(e.target.value)} placeholder="Enter refuelling liters..." />
+        </div>
+        <div className="form-control">
+        <label htmlFor="refuellingCost">Refuelling cost in euros</label>
+        <input type="number" value={refuellingCost} onChange={(e) =>
+            setRefuellingCost(e.target.value)} placeholder="Enter refuelling cost..." />
+        </div>
 
-                <button className="btn">Add trip</button>
-            </form>
+        <div className="form-control">
+        <label htmlFor="distanceDrivenWithRefuelling">Distance driven with refuelling</label>
+        <input type="number" value={distanceDrivenWithRefuelling} onChange={(e) =>
+            setDistanceDrivenWithRefuelling(e.target.value)} placeholder="Enter distance driven with refuelling..." />
+        </div>
         </>
+        ) 
+        : 
+        (
+        <>
+        <h4>Electric cars</h4>
+        <div>
+        <label htmlFor="chargingAmount">Recharing amount in kWh</label>
+        <input type="number" value={RechargingInkWh} onChange={(e) =>
+            setechargingInkWh(e.target.value)} placeholder="Enter charing amount in kWh..." />
+        </div>
+        <div>
+        <label htmlFor="chargingCost">Charging cost in euros</label>
+        <input type="number" value={ChargingCost} onChange={(e) =>
+            setChargingCost(e.target.value)} placeholder="Enter charging cost..." />
+        </div>
+        <div>
+        <label htmlFor="DistanceDrivenWithCharge">Distance driven with a charge</label>
+        <input type="number" value={distanceDrivenWithCharging} onChange={(e) =>
+            setdistanceDrivenWithCharging(e.target.value)} placeholder="Enter distance driven with a charge..." />
+        </div>
+        </>
+        )}            
+    <button className="btn">Add trip</button>
+    </form>
+    </>
     )
 }
--- src/components/TripList.js
+++ src/components/TripList.js
@@ -24,7 +24,13 @@ export const TripList = () => {
     return (
         <>
             <h3>Trips for all cars: {trips.length}</h3>
-            {trips.length <= 0 && (
+            {trips.length > 0 ? (
+                <ul className="list">
+                    {(showAll ? trips : trips.slice(0, 3)).map(trip => (
+                        <Trip key={trip.id} trip={trip} />
+                    ))}
+                </ul>
+            ) : (
                 <h5>There are no trips.</h5>
             )}
             {trips.length > 3 && (
